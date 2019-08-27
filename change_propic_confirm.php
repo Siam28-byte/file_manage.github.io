306217447ac509382a979381b6f4c3f60be960cf
joy2362 <abdullahzahidjoy@gmail.com>
2019-08-28 01:35:30 +0600

    2.3.1

---
+++ change_propic_confirm.php
@@ -0,0 +1,27 @@
+<?php 
+require("connection.php");
+error_reporting(0);
+session_start();
+$uname=$_SESSION['uname'];
+$query="select * from members where username='$uname'";
+$data=mysqli_query($conn,$query);
+$profile=mysqli_fetch_assoc($data);
+$name= $_FILES["fileup"]["name"];
+if ($name) {
+	if ($profile['propic']!="propic/female.png" && $profile['propic']!="propic/male.png") {
+		unlink($profile['propic']);
+	}
+	$temp_name= $_FILES["fileup"]["tmp_name"];
+	$folder="propic/".$name;
+	move_uploaded_file($temp_name, $folder);
+	$query="UPDATE members SET propic='$folder' WHERE username='$uname'";
+		$data=mysqli_query($conn , $query);
+		if($data){
+			header('location:index.php');
+		}else{
+			header('location:upload.php?er=1');
+		}
+}else{
+	header('location:upload.php?er=2');
+}
+?>
